16857c77669251c7ff2633ba43c75781944f82df
sasi <sasitharani@gmail.com>
2024-11-21 17:30:27 +0530

    connect frontend backend8

--- server.js
+++ server.js
@@ -239,7 +239,7 @@ app.delete('/api/invoices/:id', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
