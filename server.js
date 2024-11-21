9602873487490f4ce69fa9557bc9031692d57a9e
sasi <sasitharani@gmail.com>
2024-11-21 16:41:44 +0530

    connnecting frontend to backend7

--- server.js
+++ server.js
@@ -122,9 +122,9 @@ app.delete('/api/invoices/:id', (req, res) => {
 });
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
